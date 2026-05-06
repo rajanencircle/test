73456c2b8a8a0a2fcd95cab39020e5355d374065
rajanencircle <rajanp.encircle@gmail.com>
2026-05-06 18:06:47 +0530

    Update Notes.js phone add

--- models/Notes.js
+++ models/Notes.js
@@ -7,6 +7,12 @@ const NotesSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
+
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
