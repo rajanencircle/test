80d10c5ec37f7707fb4ca95455d829c605b2cac2
rajanencircle <rajanp.encircle@gmail.com>
2026-05-06 18:05:54 +0530

    Update Notes.js

--- models/Notes.js
+++ models/Notes.js
@@ -7,18 +7,14 @@ const NotesSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
-    content: {
-      type: String,
-      required: true,
-      trim: true,
-    },
+
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 module.exports = mongoose.model("Notes", NotesSchema);
